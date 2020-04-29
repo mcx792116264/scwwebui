643d55e9a12af706bbd5e7b79277d54c09bdeb4b
mcx <792116264@qq.com>
2020-04-29 22:32:05 +0800

    刚开始上班

--- src/main/java/com/atguigu/scwsts/common/vo/BaseVo.java
+++ src/main/java/com/atguigu/scwsts/common/vo/BaseVo.java
@@ -2,7 +2,14 @@ package com.atguigu.scwsts.common.vo;
 
 import lombok.Data;
 
+import java.sql.SQLOutput;
+
 @Data
 public class BaseVo {
 	public String accessToken;
+
+	public static void main(String[] args) {
+		System.out.println("开始上班");
+	}
+
 }
